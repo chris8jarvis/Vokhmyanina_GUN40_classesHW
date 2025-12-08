b22cc24e3dbc2c3a4e27d13a1ff34b56efa0ee97
chris8jarvis <christinaeduardovna8@gmail.com>
2025-12-08 15:26:29 +0300

    class Unit done

--- ClassesHW/Program.cs
+++ ClassesHW/Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your Unit name");
+            string name = Console.ReadLine();
+            Unit TestUnit = new Unit(name);
+            Console.WriteLine($"Hello {TestUnit.Name}");
+            Console.WriteLine($"{TestUnit.Name} your current health is {TestUnit.GetRealHealth()}");
+
+            int damage = 0;
+            bool isValidInput = false;
+
+            while (!isValidInput)
+            {
+                Console.Write("Enter your damage: ");
+                string damageInput = Console.ReadLine();
+
+                if (int.TryParse(damageInput, out damage))
+                {
+                    Console.WriteLine($"Your chosen damage is {damage}");
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number.");
+                }
+            }
+
+            bool isDead = TestUnit.SetDamage(damage);
+            if (isDead)
+            {
+                Console.WriteLine("Your Unit is dead");
+                return;
+            }
+            Console.WriteLine("Your Unit survived");
+            Console.WriteLine($"New Real Health: {TestUnit.GetRealHealth()}");
+
+
+
+            //Unit object1 = new Unit();
+            //Console.WriteLine(object1.Name);
+
+            //Console.WriteLine(object1.Damage);
+            //Console.WriteLine(object1.Health);
+            //Console.WriteLine(object1.Armor);
+
+            //Console.WriteLine(object1.GetRealHealth());
+            //Console.WriteLine(object1.SetDamage(200f));
+
+
+
+            //Unit object2 = new Unit("Foo");
+            //Console.WriteLine(object2.Name);
+            //Console.WriteLine(object2.Damage);
+            //Console.WriteLine(object2.Health);
+            //Console.WriteLine(object2.Armor);
+
         }
     }
 }
--- ClassesHW/Unit.cs
+++ ClassesHW/Unit.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ClassesHW
 {
     internal class Unit
     {
-        private float _health;
-        private int _damage = 5;
-        private float _armor = 0.6f;
 
-        public string name
+        private float _health;
+        public string Name
         {
             get;
         }
+
         public float Health
         {
             get { return _health; }
@@ -27,18 +27,79 @@ namespace ClassesHW
         }
         public float Armor
         {
-            get { return _armor; }
+            get;
         }
 
+        // конструкторы
+        // solution #1 - the best one.
+        //public Unit(string name = "Unknown Unit")
+        //{
+        //    Name = name;
+        //    Damage = 5;
+        //    Armor = 0.6f;
+        //    _health = 100;
+        //}
 
+        // solution #2 - according blue prints.
+        //public Unit() : this("Unknown Unit") {}
+        //public Unit(string name)
+        //{
+        //    Name = name;
+        //    Damage = 5;
+        //    Armor = 0.6f;
+        //    _health = 100;
+        //}
+
+
+        // solution #3 - using this, but name rewritten if declared.
         public Unit()
         {
-            this.name = "Unknown Unit";
+            Name = "Unknown Unit";
+            Damage = 5;
+            Armor = 0.6f;
+            _health = 100;
         }
-        public Unit(string name)
+
+        public Unit(string name) :this()
         {
-            this.name;
+            Name = name;
         }
+
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor); // 100(1+0.6)
+        }
+
+        //public bool SetDamage(float value)
+        //{ 
+        //    if (Health <= 0f)
+        //    {
+        //        _health = Health - value * Armor;
+        //        return true;
+        //    }
+        //    _health = Health - value * Armor;
+        //    return false;
+           
+        //}
+
+        public bool SetDamage(float value)
+        {
+            _health = Health - value * Armor;
+
+            if (Health <= 0f)
+            {
+                return true;
+            }
+            return false;
+        }
+        //public bool SetDamage(float value)
+        //{
+        //    _health = Health - value * Armor;
+        //    return Health <= 0f;
+        //}
+
     }
 
-}
+    }
+
+

111eb1fb900f229d362f79c2e8d2b85cfdcb94b2
chris8jarvis <christinaeduardovna8@gmail.com>
2025-12-09 16:12:02 +0300

    first try

--- ClassesHW/memoryHomework.cs
+++ ClassesHW/memoryHomework.cs
@@ -6,7 +6,62 @@ using System.Threading.Tasks;
 
 namespace ClassesHW
 {
-    internal class memoryHomework
+    internal class Weapon
     {
+        public string Name
+        {
+            get;
+        }
+        public int MinDamage
+        {
+            get; private set;
+        }
+        public int MaxDamage
+        {
+            get; private set;
+        }
+
+        public float Durability
+        {
+            get;
+        }
+        public Weapon()
+        {
+            Durability = 1f;
+        }
+        public Weapon(string name)
+        {
+            Name = name;
+        }
+        public Weapon(string name, int minDamage, int maxDamage) : this(name)
+        {
+            MinDamage = minDamage;
+            MaxDamage = maxDamage;
+            SetDamageParams(minDamage, maxDamage);
+        }
+        public void SetDamageParams(int minDamage, int maxDamage)
+        {
+            if (minDamage > maxDamage)
+            {
+                //числа меняются местами
+                minDamage = maxDamage;
+                maxDamage = minDamage;
+                Console.WriteLine($"Incorrect input");
+            }
+            else if (minDamage < 1)
+            {
+                //минимальный урон оружия задается значением f
+                minDamage = ;
+                Console.WriteLine("Forced setting of the minimum value");
+            }
+            else if (maxDamage <= 1)
+            {
+                maxDamage = 10;
+            }
+        }
+        public int GetDamage()
+        {
+            return (MaxDamage - MinDamage) / 2;
+        }
     }
 }

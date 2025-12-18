85d3f1e2a28f1775cba465e61f72d99e0f463965
chris8jarvis <christinaeduardovna8@gmail.com>
2025-12-18 16:22:11 +0300

    task 1 draft

--- ClassesHW/Collections.cs
+++ ClassesHW/Collections.cs
@@ -1,10 +1,34 @@
 ﻿namespace ClassesHW
 {
-    internal class Program
+    internal class Collections
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Task 1");
+            Console.WriteLine();
+
+            var shoppingList = new List<string>() {"toilet paper", "bread", "milk" };
+            Console.WriteLine("Items in your basket:");
+            Console.WriteLine(string.Join(", ", shoppingList));
+            //for(int i = 0; i < shoppingList.Count; i++)
+            //{
+            //    Console.WriteLine(shoppingList[i]);
+            //}
+            Console.WriteLine();
+            Console.WriteLine("Add an item you need to buy in a shop");
+            string item = Console.ReadLine();
+            shoppingList.Add(item);
+            Console.WriteLine();
+            Console.WriteLine("Updated items in your basket: ");
+            Console.WriteLine(string.Join(", ", shoppingList));
+            Console.WriteLine();
+            Console.WriteLine("Add one more item you need to buy in a shop");
+            string item2 = Console.ReadLine();
+            int middleIndex = shoppingList.Count / 2;
+            shoppingList.Insert(middleIndex, item2);
+            Console.WriteLine();
+            Console.WriteLine("Updated items in your basket: ");
+            Console.WriteLine(string.Join(", ", shoppingList));
         } 
     } 
 }

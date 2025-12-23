dd839d0a64555109da5732d5bc3305dde6655c88
chris8jarvis <christinaeduardovna8@gmail.com>
2025-12-23 15:46:23 +0300

    task 3 in progress

---
+++ ClassesHW/3TaskNode.cs
@@ -0,0 +1,15 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ClassesHW
+//{
+//    internal class _3TaskNode
+//    {
+//        public string Value;
+
+//        public _3TaskNode Next;
+//    }
+//}
--- ClassesHW/Collections.cs
+++ ClassesHW/Collections.cs
@@ -31,49 +31,76 @@
             //Console.WriteLine(string.Join(", ", shoppingList));
 
             ////////////////////////////////////////////////////////////////////////
-            Console.WriteLine("Task 2");
-            Console.WriteLine("Input student name");
-            string studentName = Console.ReadLine();
-            int studentGrade = 0;
-            bool isValidInput = false;
+            //Console.WriteLine("Task 2");
+            //Console.WriteLine("Input student name");
+            //string studentName = Console.ReadLine();
+            //int studentGrade = 0;
+            //bool isValidInput = false;
 
 
-            while (!isValidInput)
+            //while (!isValidInput)
+            //{
+            //    Console.Write("Input student grade: ");
+            //    string input = Console.ReadLine();
+
+
+            //    if (int.TryParse(input, out studentGrade))
+            //    {
+            //        if (studentGrade >= 2 && studentGrade <= 5)
+            //        {
+
+            //            isValidInput = true;
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Grade must be from 2 to 5");
+            //        }
+            //    }
+            //}
+            //Console.WriteLine($"Student: {studentName}, Grade: {studentGrade}");
+
+            //var studentsBook = new Dictionary<string, int>();
+            //studentsBook.Add(studentName, studentGrade);
+
+            //Console.WriteLine("Input student name to find grade:");
+            //string searchName = Console.ReadLine();
+
+            //if (studentsBook.ContainsKey(searchName))
+            //{
+            //    int foundGrade = studentsBook[searchName];
+            //    Console.WriteLine($"Student {searchName} has grade {foundGrade}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"Error: Student '{searchName}' does not exist in the list.");
+            //}
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("Task 3");
+
+            Console.WriteLine("Create a wish-list containing from 3 to 6 elements.");
+            string element1 = Console.ReadLine();
+            string element2 = Console.ReadLine();
+            string element3 = Console.ReadLine();
+            string element4 = Console.ReadLine();
+            string element5 = Console.ReadLine();
+            string element6 = Console.ReadLine();
+
+
+            LinkedList<string> wishlist = new LinkedList<string>(new[] { element1, element2, element3, element4, element5, element6 });
+
+            var currentNode = wishlist.First;
+            while (currentNode != null)
             {
-                Console.Write("Input student grade: ");
-                string input = Console.ReadLine();
-
-
-                if (int.TryParse(input, out studentGrade))
-                {
-                    if (studentGrade >= 2 && studentGrade <= 5)
-                    {
-                        
-                        isValidInput = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Grade must be from 2 to 5");
-                    }
-                }
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next;
             }
-            Console.WriteLine($"Student: {studentName}, Grade: {studentGrade}");
 
-            var studentsBook = new Dictionary<string, int>();
-            studentsBook.Add(studentName, studentGrade);
-
-            Console.WriteLine("Input student name to find grade:");
-            string searchName = Console.ReadLine();
-
-            if (studentsBook.ContainsKey(searchName))
+            currentNode = wishlist.Last;
+            while (currentNode != null)
             {
-                int foundGrade = studentsBook[searchName];
-                Console.WriteLine($"Student {searchName} has grade {foundGrade}");
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Previous;
             }
-            else
-            {
-                Console.WriteLine($"Error: Student '{searchName}' does not exist in the list.");
-            }
-        } 
+        }
     }
 }

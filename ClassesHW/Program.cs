174b71e58b7fad7629c2ac59e625537caca977d6
chris8jarvis <christinaeduardovna8@gmail.com>
2025-12-26 15:34:31 +0300

    task with examples

--- ClassesHW/Program.cs
+++ ClassesHW/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml.Serialization;
 
 namespace ClassesHW
 {
@@ -7,7 +8,8 @@ namespace ClassesHW
         static void Main(string[] args)
         {
             Console.WriteLine("Enter 1, 2 or 3 to check task 1, 2 or 3");
-            int task = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int.TryParse(input, out int task);
 
             switch (task)
             {
@@ -15,114 +17,15 @@ namespace ClassesHW
                     CheckFirstTask();
                     break;
                 case 2:
-                    CheckSecondTask(); 
+                    CheckSecondTask();
                     break;
                 case 3:
                     CheckThirdTask();
                     break;
+                default:
+                    Console.WriteLine("Input is incorrect");
+                    break;
             }
-
-            //Console.WriteLine("Task 1");
-
-            //var shoppingList = new List<string>() { "toilet paper", "bread", "milk" };
-            //Console.WriteLine("Items in your basket:");
-            //Console.WriteLine(string.Join(", ", shoppingList));
-            ////for(int i = 0; i < shoppingList.Count; i++)
-            ////{
-            ////    Console.WriteLine(shoppingList[i]);
-            ////}
-            //Console.WriteLine();
-            //Console.WriteLine("Add an item you need to buy in a shop");
-            //string item = Console.ReadLine();
-            //shoppingList.Add(item);
-            //Console.WriteLine();
-            //Console.WriteLine("Updated items in your basket: ");
-            //Console.WriteLine(string.Join(", ", shoppingList));
-            //Console.WriteLine();
-            //Console.WriteLine("Add one more item you need to buy in a shop");
-            //string item2 = Console.ReadLine();
-            //int middleIndex = shoppingList.Count / 2;
-            //shoppingList.Insert(middleIndex, item2);
-            //Console.WriteLine();
-            //Console.WriteLine("Updated items in your basket: ");
-            //Console.WriteLine(string.Join(", ", shoppingList));
-
-            //////////////////////////////////////////////////////////////////////
-            //Console.WriteLine();
-            //Console.WriteLine("Task 2");
-            //Console.WriteLine("Input student name");
-            //string studentName = Console.ReadLine();
-            //int studentGrade = 0;
-            //bool isValidInput = false;
-
-
-            //while (!isValidInput)
-            //{
-            //    Console.Write("Input student grade: ");
-            //    string input = Console.ReadLine();
-
-
-            //    if (int.TryParse(input, out studentGrade))
-            //    {
-            //        if (studentGrade >= 2 && studentGrade <= 5)
-            //        {
-
-            //            isValidInput = true;
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine("Grade must be from 2 to 5");
-            //        }
-            //    }
-            //}
-            //Console.WriteLine($"Student: {studentName}, Grade: {studentGrade}");
-
-            //var studentsBook = new Dictionary<string, int>();
-            //studentsBook.Add(studentName, studentGrade);
-
-            //Console.WriteLine("Input student name to find grade:");
-            //string searchName = Console.ReadLine();
-
-            //if (studentsBook.ContainsKey(searchName))
-            //{
-            //    int foundGrade = studentsBook[searchName];
-            //    Console.WriteLine($"Student {searchName} has grade {foundGrade}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"Error: Student '{searchName}' does not exist in the list.");
-            //}
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //Console.WriteLine();
-            //Console.WriteLine("Task 3");
-
-            //Console.WriteLine("Create a wish-list containing from 3 to 6 elements.");
-            //string element1 = Console.ReadLine();
-            //string element2 = Console.ReadLine();
-            //string element3 = Console.ReadLine();
-            //string element4 = Console.ReadLine();
-            //string element5 = Console.ReadLine();
-            //string element6 = Console.ReadLine();
-            //Console.WriteLine();
-
-
-            //LinkedList<string> wishlist = new LinkedList<string>(new[] { element1, element2, element3, element4, element5, element6 });
-
-            //var currentNode = wishlist.First;
-            //while (currentNode != null)
-            //{
-            //    Console.WriteLine(currentNode.Value);
-            //    currentNode = currentNode.Next;
-                
-            //}
-            //Console.WriteLine();
-
-            //currentNode = wishlist.Last;
-            //while (currentNode != null)
-            //{
-            //    Console.WriteLine(currentNode.Value);
-            //    currentNode = currentNode.Previous;
-            //}
         }
 
         private class ListTask
@@ -133,24 +36,25 @@ namespace ClassesHW
             {
                 _shoppingList = new List<string>() { "toilet paper", "bread", "milk" };
 
-                Console.WriteLine("Items in your basket:");
+                Console.WriteLine("Items in your basket: ");
                 Console.WriteLine(string.Join(", ", _shoppingList));
                 Console.WriteLine();
 
-                Console.WriteLine("Add an item you need to buy in a shop");
-                string item = Console.ReadLine();
-                _shoppingList.Add(item);
+                Console.Write("Add an item you need to buy in a shop: ");
+                string inputItemToAddInEnd = Console.ReadLine();
+                _shoppingList.Add(inputItemToAddInEnd);
                 Console.WriteLine();
 
                 Console.WriteLine("Updated items in your basket: ");
                 Console.WriteLine(string.Join(", ", _shoppingList));
                 Console.WriteLine();
 
-                Console.WriteLine("Add one more item you need to buy in a shop");
-                string item2 = Console.ReadLine();
+                Console.Write("Add one more item you need to buy in a shop: ");
+                string inputItemToAddInMiddle = Console.ReadLine();
+                Console.WriteLine();
+
                 int middleIndex = _shoppingList.Count / 2;
-                _shoppingList.Insert(middleIndex, item2);
-                Console.WriteLine();//?
+                _shoppingList.Insert(middleIndex, inputItemToAddInMiddle);
                 Console.WriteLine("Updated items in your basket: ");
                 Console.WriteLine(string.Join(", ", _shoppingList));
 
@@ -160,71 +64,106 @@ namespace ClassesHW
         private class DictionaryTask
         {
             private Dictionary<string, int> _studentsBook;
+            public DictionaryTask()
+            {
+                _studentsBook = new Dictionary<string, int>();
+            }
 
             public void TaskLoop()
             {
-                Console.WriteLine("Input student name");
-                string studentName = Console.ReadLine();
-                int studentGrade = 0;
-                bool isValidInput = false;
+                int studentGrade = 2;
+                int maxStudents = 30;
+                int studentsCounter = 0;
 
-
-                while (!isValidInput)
+                while (studentsCounter < maxStudents)
                 {
-                    Console.Write("Input student grade: ");
+                    Console.Write("Input student name (or 'exit' to finish): ");
                     string input = Console.ReadLine();
-
-
-                    if (int.TryParse(input, out studentGrade))
+                    if (input == "exit")
                     {
-                        if (studentGrade >= 2 && studentGrade <= 5)
-                        {
+                        break;
+                    }
+                    string inputStudentName = input;
 
-                            isValidInput = true;
+                    bool isValidInput = false;
+                    while (!isValidInput)
+                    {
+                        Console.Write("Input student grade: ");
+                        string inputGrade = Console.ReadLine();
+
+
+                        if (int.TryParse(inputGrade, out studentGrade))
+                        {
+                            if (studentGrade >= 2 && studentGrade <= 5)
+                            {
+
+                                isValidInput = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Grade must be from 2 to 5");
+                            }
                         }
                         else
                         {
-                            Console.WriteLine("Grade must be from 2 to 5");
+                            Console.WriteLine("Incorrect input. Please write a number");
                         }
                     }
+
+                    if (!_studentsBook.TryAdd(inputStudentName, studentGrade))
+                    {
+                        Console.WriteLine($"Student {inputStudentName} already exsist");
+                        continue;
+                    }
+                    studentsCounter++;
                 }
-                Console.WriteLine($"Student: {studentName}, Grade: {studentGrade}");
 
-                _studentsBook = new Dictionary<string, int>();
-                _studentsBook.Add(studentName, studentGrade);
+                Console.WriteLine();
+                Console.Write("Input student name to find grade: ");
+                string requiredName = Console.ReadLine(); // может Required?
 
-                Console.WriteLine("Input student name to find grade:");
-                string searchName = Console.ReadLine();
-
-                if (_studentsBook.ContainsKey(searchName))
+                if (_studentsBook.ContainsKey(requiredName))
                 {
-                    int foundGrade = _studentsBook[searchName];
-                    Console.WriteLine($"Student {searchName} has grade {foundGrade}");
+                    int foundGrade = _studentsBook[requiredName];
+                    Console.WriteLine($"Student {requiredName} has grade {foundGrade}");
                 }
                 else
                 {
-                    Console.WriteLine($"Error: Student '{searchName}' does not exist in the list.");
+                    Console.WriteLine($"Error: Student '{requiredName}' does not exist in the list.");
                 }
             }
         }
-
+    
         private class LinkedListTask
         {
             private LinkedList<string> _wishList;
 
             public void TaskLoop()
             {
+
                 Console.WriteLine("Create a wish-list containing from 3 to 6 elements.");
-                string element1 = Console.ReadLine();
-                string element2 = Console.ReadLine();
-                string element3 = Console.ReadLine();
-                string element4 = Console.ReadLine();
-                string element5 = Console.ReadLine();
-                string element6 = Console.ReadLine();
-                Console.WriteLine();
 
+                _wishList = new LinkedList<string>();
+                int maxWishes = 6;
+                int minWishes = 3;
 
-                _wishList = new LinkedList<string>(new[] { element1, element2, element3, element4, element5, element6 });
+                while(_wishList.Count <= maxWishes)
+                {
+                    Console.Write("Add wish or 'exit' to finish: ");
+                    string userInput = Console.ReadLine();
+
+                    if (userInput == "exit")
+                    {
+                        if (_wishList.Count < minWishes)
+                        {
+                            Console.WriteLine($"You have to add at least {minWishes} wishes");
+                            continue;
+                        }
+                        break;
+                    }
+                    
+                    _wishList.AddLast(userInput);
+                }
 
                 var currentNode = _wishList.First;
                 while (currentNode != null)
@@ -258,5 +197,83 @@ namespace ClassesHW
             var linkedListTask = new LinkedListTask();
             linkedListTask.TaskLoop();
         }
+
+
+        private static void TestHowToBreakLoops()
+        {
+            // #1
+            int targetSum = 100;
+            int sum = 0;
+            int age = 0;
+            while (sum <= targetSum || age < 100)
+            {
+                age++;
+                Console.Write("Enter coins count: ");
+                string input = Console.ReadLine();
+                int.TryParse(input, out int inputSum);
+                sum += inputSum;
+            }
+
+            // #2
+            int userCount = 5;
+            int i = 0;
+            while(true)
+            {
+
+                Console.Write("Enter the Name or 'stop' for exit: ");
+                string input = Console.ReadLine();
+
+                if (input == "stop") {
+                    break;
+                }
+                Console.WriteLine($"Hello {input} ");
+
+                i++;
+                if (i == userCount)
+                {
+                    break;
+                }
+            }
+
+            // #3
+            int stCount = 5;
+            int j = 0;
+            bool exitCondition = false;
+            while (!exitCondition)
+            {
+
+                Console.Write("Enter the Name or 'stop' for exit: ");
+                string input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    break;
+                }
+                j++;
+                Console.WriteLine($"Hello {input} ");
+
+                exitCondition = j == stCount;
+            }
+
+            // #4
+            int blCount = 5;
+            int k = 0;
+            while (k < blCount)
+            {
+
+                Console.Write("Enter the Name or 'stop' for exit: ");
+                string input = Console.ReadLine();
+
+                if (input == "stop")
+                {
+                    break;
+                }
+                k++;
+                Console.WriteLine($"Hello {input} ");
+
+            }
+
+        }
+
     }
 }
